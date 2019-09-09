a186439dea9be4f19d8ea7e5910b1cd04adbafb3
histo <historiabdevil@gmail.com>
2019-09-09 15:22:33 +0900

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import {Component} from '@angular/core';
 import {ElectronService} from './core/services';
 import {TranslateService} from '@ngx-translate/core';
 import {AppConfig} from '../environments/environment';
-import * as url from "url";
-import * as path from "path";
+import * as url from 'url';
+import * as path from 'path';
 
 @Component({
   selector: 'app-root',
@@ -27,6 +27,24 @@ export class AppComponent {
     } else {
       console.log('Mode web');
     }
+    this.electronService.fs.exists('./setting.json', (exists) => {
+      if (!exists) {
+        this.electronService.fs.writeFileSync('./setting.json',
+          '{"serviceid":"hdyang@catenoid.net","servicekey":"21c728d5182241392c20021c33d3a604c3e5380076406d11","user":"admin"}');
+      }
+    });
+    this.electronService.fs.exists('./caster.json', (exists) => {
+      if (!exists) {
+        this.electronService.fs.writeFileSync('./caster.json',
+          '{"resolution":"1920X1080","frameRate":"30","codec":"H264","bitrate":"4000"}');
+      }
+    });
+    this.electronService.fs.exists('./branch-caster.json', (exists) => {
+      if (!exists) {
+        this.electronService.fs.writeFileSync('./branch-caster.json',
+          '{"resolution":"1920X1080","frameRate":"30","codec":"VP9","bitrate":"1500"}');
+      }
+    });
   }
 
   onViwerNewWinodw($event: MouseEvent, link: string) {
@@ -37,11 +55,16 @@ export class AppComponent {
       resizable: false,
       frame: true,
       transparent: false,
-      webPreferences:{
+      webPreferences: {
         nodeIntegration: true
       }
     });
-    win.loadURL('file://' + __dirname + '/index.html#/' + link);
+    win.loadURL(
+      'file://' +
+      __dirname +
+      '/index.html#/' +
+      link
+    );
     console.log(win);
   }
 }
